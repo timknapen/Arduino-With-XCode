f97132cc647d02042b8ed40080e677e5675187cf
Tim Knapen <timknapen@gmail.com>
2011-11-12 15:39:01 +0100

    adding readme file

---
+++ Arduino/SanguinoOnXcode2/.dep/main.o.d
@@ -0,0 +1,19 @@
+main.o: main.c \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/io.h \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/sfr_defs.h \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/inttypes.h \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/stdint.h \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/iom169.h \
+  /sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/portpins.h
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/io.h:
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/sfr_defs.h:
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/inttypes.h:
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/stdint.h:
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/iom169.h:
+
+/sw/share/avr/lib/gcc/avr/4.0.2/../../../../avr/include/avr/portpins.h:
