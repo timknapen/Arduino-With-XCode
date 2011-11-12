0e25d17e986d0d91ef527ed41dc63164f7bdc4cd
Tim Knapen <timknapen@gmail.com>
2011-11-12 15:39:01 +0100

    adding readme file

---
+++ Arduino/SanguinoOnXcode2/src/TestThing.cpp
@@ -0,0 +1,7 @@
+
+#include "TestThing.h"
+
+
+void TestThing::setup(){
+	Serial.println("test thing is setup again!");
+}
---
+++ Arduino/SanguinoOnXcode2/src/TestThing.h
@@ -0,0 +1,12 @@
+#ifndef _TEST_THING_
+#define _TEST_THING_
+
+#include "WProgram.h"
+
+class TestThing{
+public:
+	void setup();
+};
+
+
+#endif
