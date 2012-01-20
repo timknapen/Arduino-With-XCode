1e597bd437017d5d3c96a742557fe119642bda8b
Tim Knapen <timknapen@gmail.com>
2012-01-20 01:57:45 +0100

    fixed code completion for XCode 4

---
+++ Arduino/ArduinoOnXCode4_3/src/ArduinoProgram.cpp
@@ -0,0 +1,31 @@
+#include "ArduinoProgram.h"
+
+void ArduinoProgram::setup(){
+	counter = 0;
+	Serial.begin(9600); 
+    
+	Serial.println("-----------------------------");
+	Serial.println("   saying hi from XCode4     ");
+    Serial.println("   ... again   ");
+	Serial.println("-----------------------------");
+	
+	//our class from our src/ folder
+	thing = new TestThing();
+	thing -> setup();
+}
+
+
+void ArduinoProgram::loop(){
+    
+	counter ++;
+    
+	Serial.print("loop nr. ");
+	Serial.println(counter, DEC);
+    
+    thing->doSomething();
+    
+    thing->anotherMethod(5);
+    
+	delay(300);	
+}
+
